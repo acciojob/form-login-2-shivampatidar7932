c384016f8029192b7f0e08c8defc1ccb3bbcd7d7
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-11 09:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function submitForm() {
+            // Get form values
+            var firstName = document.getElementById('firstName').value;
+            var lastName = document.getElementById('lastName').value;
+            var phoneNumber = document.getElementById('phoneNumber').value;
+            var email = document.getElementById('email').value;
+
+            // Create alert message with formatted text
+            var alertMessage = "First Name: " + firstName + "\n" +
+                               "Last Name: " + lastName + "\n" +
+                               "Phone Number: " + phoneNumber + "\n" +
+                               "Email ID: " + email;
+
+            // Display the alert
+            alert(alertMessage);
+        }
